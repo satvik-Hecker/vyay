0f654816075eef4468e45e0180b9ff6727443e91
Satvik Tiwari <satvikk.tiwariii@gmail.com>
2026-03-04 20:41:01 +0530

    Pagination and Filtering features implemented in backend for efficiency

--- backend/src/controllers/transaction.controller.js
+++ backend/src/controllers/transaction.controller.js
@@ -24,9 +24,51 @@ export const createTransaction = async(req, res)=> {
 //get trnsaction
 export const getTransactions = async(req,res)=> {
     try{
-        const transactions = await Transaction.find({ userId: req.user.userId})
-        .sort({ createdAt: -1});
-        res.json(transactions)
+
+        //filtering
+        const {category,type, paymentMethod,startDate, endDate}= req.query;
+        
+        const filter = {userId: req.user.userId};
+
+        if(category){
+            filter.category=category.toLowerCase()
+        }
+        if(type){
+            filter.type=type.toLowerCase()
+        }
+        if(paymentMethod){
+            filter.paymentMethod=paymentMethod.toLowerCase()
+        }
+        if(startDate || endDate){
+            filter.transactionDate={};
+        if(startDate){
+            filter.transactionDate.$gte=new Date (startDate)
+        }
+        if(endDate){
+            filter.transactionDate.$lte=new Date (endDate)
+        }
+    }
+
+        //pagination
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page-1)*limit;
+
+        const totalTransactions = await Transaction.countDocuments(filter);
+        const totalPages = Math.ceil(totalTransactions / limit);
+
+        const transactions = await Transaction.find(filter)
+        .sort({ transactionDate: -1})
+        .skip(skip)
+        .limit(limit);
+        res.json({
+            page,
+            limit,
+            totalTransactions,
+            totalPages,
+            transactions
+        });
+
     }catch(error){
         res.status(500).json({ message: error.message});
     }
