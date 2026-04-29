8f9dc61eff96cc5f5e7e1764bb045b02d7629aca
Satvik Tiwari <satvikk.tiwariii@gmail.com>
2026-04-29 22:17:22 +0530

    Testing and Fixing UI bugs

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -10,7 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0c0a09',
     icons: [
       {
-        src: '/icon-192x192.png',
+        src: '/img2.png',
         sizes: '192x192',
         type: 'image/png',
       },
